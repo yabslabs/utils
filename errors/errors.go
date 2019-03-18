f105461806c1ca4b98080a705e3fd2a6e9193c7b
louis <louis.baumann@vrsg.ch>
2019-03-18 11:13:19 +0100

    log on err panic

--- errors/errors.go
+++ errors/errors.go
@@ -1,7 +1,17 @@
 package errors
 
-import "strings"
+import (
+	"strings"
+
+	"github.com/yabslabs/utils/logging"
+)
 
 func Contains(err error, needle string) bool {
 	return err != nil && strings.Contains(err.Error(), needle)
+}
+
+func PanicErrID(id string, err error, msg ...string) {
+	if err != nil {
+		logging.Log(id).OnError(err).Panic(msg)
+	}
 }
