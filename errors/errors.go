774651c6d39d0eca6cb6942edd22383918bac03b
louis <louis.baumann@vrsg.ch>
2019-03-18 11:20:50 +0100

    remove PanicErrID because logging already satisfies this need

--- errors/errors.go
+++ errors/errors.go
@@ -2,16 +2,8 @@ package errors
 
 import (
 	"strings"
-
-	"github.com/yabslabs/utils/logging"
 )
 
 func Contains(err error, needle string) bool {
 	return err != nil && strings.Contains(err.Error(), needle)
-}
-
-func PanicErrID(id string, err error, msg ...string) {
-	if err != nil {
-		logging.Log(id).OnError(err).Panic(msg)
-	}
 }
